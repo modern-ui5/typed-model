688d9f5cc95dba52bfaa4d81837380e90ddac932
Yichuan Shen <shen.yichuan@gmail.com>
2023-07-27 18:43:24 +0200

    Refactor

--- test/loadUi5.ts
+++ test/loadUi5.ts
@@ -1,16 +1,10 @@
 const scriptTag = document.createElement("script");
-
 scriptTag.src = "https://sdk.openui5.org/resources/sap-ui-core.js";
-
-let promResolve: () => void;
-const prom = new Promise<void>((resolve) => {
-  promResolve = resolve;
-});
-
-scriptTag.onload = () => {
-  promResolve();
-};
 
 document.body.appendChild(scriptTag);
 
-await prom;
+await new Promise<void>((resolve) => {
+  scriptTag.onload = () => {
+    resolve();
+  };
+});
